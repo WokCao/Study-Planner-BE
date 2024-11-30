4681e12e1006d5bec72259129f0471b633fb283c
WokCao <henryquoccao@gmail.com>
2024-11-30 10:14:30 +0700

    Update: update module files to user entity + connect to Posgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, 
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [User],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity'
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
+  imports: [
+    TypeOrmModule.forFeature([User]) // Import orm module in order to use entity
+  ]
 })
 export class UsersModule {}
