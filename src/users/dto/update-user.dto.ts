1a7dd60dda059a26094ac45e20b89d0ad598796e
WokCao <henryquoccao@gmail.com>
2024-11-30 10:07:14 +0700

    Create: create user DTO

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,12 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { IsNotEmpty } from "class-validator";
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+    @IsNotEmpty()
+    fullname: string;
+
+    @IsNotEmpty()
+    password: string;
+
+    @IsNotEmpty()
+    avatar: string
+}
