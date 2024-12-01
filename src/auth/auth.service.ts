c4157cefa58d4e1d8339d56f18a26824d9ded93e
WokCao <henryquoccao@gmail.com>
2024-12-02 00:05:38 +0700

    Update: set token to redis when login; Create: function to handle logout account

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,23 +3,26 @@ import { JwtService } from '@nestjs/jwt';
 import { LoginUserDto } from 'src/users/dto/login-user.dto';
 import { UsersService } from 'src/users/users.service';
 import { LoginResponseDto } from './dto/login-response.dto';
+import { RedisService } from 'src/redis/redis.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService, private jwtService: JwtService) { }
+    constructor(private usersService: UsersService, private jwtService: JwtService, private readonly redisService: RedisService) { }
     async login(loginUserDto: LoginUserDto): Promise<{ loginResponseDto: LoginResponseDto, id: number }> {
         try {
             const user = await this.usersService.login(loginUserDto);
-            console.log(user)
             if (!user) {
                 throw new UnauthorizedException('Invalid email or password');
             }
 
             const { password, ...result } = user;
             const payload = { sub: result.id, username: result.username, email: result.email };
+            const token = await this.jwtService.signAsync(payload);
+            await this.redisService.saveToken(user.id, token, 120);
+
             return {
                 loginResponseDto: {
-                    token: await this.jwtService.signAsync(payload),
+                    token,
                     username: result.username,
                     email: result.email,
                     fullname: result.fullname,
@@ -33,5 +36,11 @@ export class AuthService {
             }
             throw new InternalServerErrorException(error);
         }
+    }
+
+    async logout(token: string): Promise<void> {
+        const decoded = this.jwtService.decode(token) as any;
+        const ttl = Math.floor((decoded.exp * 1000 - Date.now()) / 1000);
+        await this.redisService.blacklistToken(token, ttl);
     }
 }
