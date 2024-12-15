6e28077d4d26523bb78325cc122d7d1aa619c516
WokCao <henryquoccao@gmail.com>
2024-12-16 00:24:23 +0700

    Update: make URL public

--- src/cloud-storage/cloud-storage.service.ts
+++ src/cloud-storage/cloud-storage.service.ts
@@ -3,30 +3,40 @@ import { Storage } from '@google-cloud/storage';
 
 @Injectable()
 export class CloudStorageService {
-  private storage: Storage;
-  private bucketName: string;
+    private storage: Storage;
+    private bucketName: string;
 
-  constructor() {
-    this.storage = new Storage();
-    this.bucketName = 'study-planner';
-  }
+    constructor() {
+        this.storage = new Storage();
+        this.bucketName = 'study-planner';
+    }
 
-  async uploadFile(file: Express.Multer.File, id: number): Promise<string> {
-    const { originalname, buffer } = file;
-    const fileName = `${Date.now()}-${originalname}-${id}`;
+    async uploadFile(file: Express.Multer.File, id: number): Promise<string> {
+        const { originalname, buffer } = file;
+        const fileName = `${Date.now()}-${originalname}-${id}`;
 
-    const fileUpload = this.storage.bucket(this.bucketName).file(fileName);
-    const stream = fileUpload.createWriteStream({
-      metadata: {
-        contentType: file.mimetype,
-      },
-    });
+        const fileUpload = this.storage.bucket(this.bucketName).file(fileName);
+        const stream = fileUpload.createWriteStream({
+            metadata: {
+                contentType: file.mimetype,
+            },
+        });
 
-    return new Promise((resolve, reject) => {
-      stream.on('error', (err) => reject(err));
-      stream.on('finish', () => resolve(fileUpload.publicUrl()));
+        return new Promise((resolve, reject) => {
+            stream.on('error', (err) => reject(err));
+            stream.on('finish', async () => {
+                try {
+                    await fileUpload.acl.update({
+                        entity: 'allUsers',
+                        role: 'READER'
+                    });
+                    resolve(fileUpload.publicUrl());
+                } catch (error) {
+                    reject(error);
+                }
+            });
 
-      stream.end(buffer);
-    });
-  }
+            stream.end(buffer);
+        });
+    }
 }
