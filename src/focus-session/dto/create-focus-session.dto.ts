1391e6fdc245df908de4d06c3523eebdda13542b
WokCao <henryquoccao@gmail.com>
2024-12-31 11:27:03 +0700

    Create: focus session dto

--- src/focus-session/dto/create-focus-session.dto.ts
+++ src/focus-session/dto/create-focus-session.dto.ts
@@ -1,3 +1,9 @@
+import { IsEnum, IsNotEmpty } from "class-validator";
+
 export class CreateFocusSessionDto {
-    
+    @IsNotEmpty()
+    taskId: number;
+
+    @IsEnum(['Completed', 'Ended early', 'Idle'], { message: 'status must be Completed, Ended early, or Idle' })
+    status: 'Completed' | 'Ended early' | 'Idle';
 }
