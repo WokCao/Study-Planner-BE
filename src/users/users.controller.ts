773375ea3bf45246fccf064477f69bbffee8b8da
WokCao <henryquoccao@gmail.com>
2025-01-05 16:10:28 +0700

    Update: appropriate error type + surround with trycatch + move logout to user service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Delete, Put, NotFoundException, UnauthorizedException, InternalServerErrorException, UseGuards, Req, ValidationPipe, ForbiddenException, BadRequestException, UseInterceptors, UploadedFile, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Delete, Put, NotFoundException, UnauthorizedException, InternalServerErrorException, UseGuards, Req, ValidationPipe, ForbiddenException, BadRequestException, UseInterceptors, UploadedFile, Param, Headers, NotImplementedException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -10,135 +10,187 @@ import { ResetPasswordDto } from './dto/reset-password.dto';
 
 @Controller('api/v1/users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService, private readonly cloudStorageService: CloudStorageService) { }
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly cloudStorageService: CloudStorageService) { }
 
-  @Post()
-  async create(@Body(new ValidationPipe()) createUserDto: CreateUserDto) {
-    try {
-      const createdUser: User = await this.usersService.create(createUserDto);
-      const { id, password, updatedAt, createdAt, ...response } = createdUser;
-      return {
-        data: {
-          response,
-          ref: `https://study-planner-be.onrender.com/api/v1/users/${createdUser.id}`
-        },
-        statusCode: 201,
-        message: 'Account has been successfully created'
-      }
-    } catch (error: any) {
-      if (error.statusCode === 409) {
-        throw new UnauthorizedException(error.message);
-      } else if (error.statusCode === 500) {
-        throw new InternalServerErrorException(error.message);
-      } else {
-        throw new BadRequestException(error.message);
-      }
+    @Post()
+    async create(@Body(new ValidationPipe()) createUserDto: CreateUserDto) {
+        try {
+            const createdUser: User = await this.usersService.create(createUserDto);
+            const { id, password, updatedAt, createdAt, ...response } = createdUser;
+            return {
+                data: {
+                    response,
+                    ref: `https://study-planner-be.onrender.com/api/v1/users/${createdUser.id}`
+                },
+                statusCode: 201,
+                message: 'Account has been successfully created'
+            }
+        } catch (error: any) {
+            if (error.statusCode === 409) {
+                throw new UnauthorizedException(error.message);
+            } else if (error.statusCode === 500) {
+                throw new InternalServerErrorException(error.message);
+            } else if (error.statusCode === 501) {
+                throw new NotImplementedException(error.message);
+            } else {
+                throw new BadRequestException(error.message);
+            }
+        }
     }
-  }
 
-  @Get('activate/:token')
-  async activateAccount(@Param('token') token: string) {
-    try {
-      await this.usersService.activateAccount(token);
-      return '<p>Your account has been activated successfully. Back to <a href="http://localhost:5173/Study-Planner-FE/login">Login</a> page.</p>'
-    } catch (error: any) {
-      if (error.statusCode === 500) {
-        throw new InternalServerErrorException(error.message);
-      } else {
-        throw new BadRequestException(error.message);
-      }
+    @Get('activate/:token')
+    async activateAccount(@Param('token') token: string) {
+        try {
+            await this.usersService.activateAccount(token);
+            return '<p>Your account has been activated successfully. Back to <a href="http://localhost:5173/Study-Planner-FE/login">Login</a> page.</p>'
+        } catch (error: any) {
+            if (error.statusCode === 404) {
+                throw new NotFoundException(error.message);
+            } else if (error.statusCode === 500) {
+                throw new InternalServerErrorException(error.message);
+            } else {
+                throw new BadRequestException(error.message);
+            }
+        }
     }
-  }
 
-  @Post('reset-password')
-  async resetPassword(@Body(new ValidationPipe()) body: ResetPasswordDto) {
-    try {
-      await this.usersService.resetPassword(body.email);
-      return {
-        statusCode: 200,
-        message: 'New password has been sent to your email. Please change it after having logged in'
-      }
-    } catch (error: any) {
-      if (error.statusCode === 500) {
-        throw new InternalServerErrorException(error.message);
-      } else {
-        throw new BadRequestException(error.message);
-      }
+    @Post('reset-password')
+    async resetPassword(@Body(new ValidationPipe()) body: ResetPasswordDto) {
+        try {
+            await this.usersService.resetPassword(body.email);
+            return {
+                statusCode: 200,
+                message: 'New password has been sent to your email. Please change it after having logged in'
+            }
+        } catch (error: any) {
+            if (error.statusCode === 401) {
+                throw new UnauthorizedException(error.message);
+            } else if (error.statusCode === 500) {
+                throw new InternalServerErrorException(error.message);
+            } else {
+                throw new BadRequestException(error.message);
+            }
+        }
     }
-  }
 
-  @UseGuards(AuthenGuard)
-  @Get()
-  async findOne(@Req() req: any) {
-    try {
-      const user: User = await this.usersService.findOne(req.user.sub);
-      const { id, password, updatedAt, createdAt, ...response } = user;
-      return {
-        "data": {
-          response
-        },
-        "statusCode": 200,
-        "message": 'Successfully'
-      }
-    } catch (error: any) {
-      throw new NotFoundException(`User can't be found`);
+    @UseGuards(AuthenGuard)
+    @Get()
+    async findOne(@Req() req: any) {
+        try {
+            const user: User = await this.usersService.findOne(req.user.sub);
+            const { id, password, updatedAt, createdAt, ...response } = user;
+            return {
+                data: {
+                    response
+                },
+                statusCode: 200,
+                message: 'Successfully'
+            }
+        } catch (error: any) {
+            if (error.statusCode === 500) {
+                throw new InternalServerErrorException(error.message);
+            } else {
+                throw new BadRequestException(error.message);
+            }
+        }
     }
-  }
 
-  @UseGuards(AuthenGuard)
-  @Put()
-  async update(@Req() req: any, @Body(new ValidationPipe({ whitelist: true })) updateUserDto: UpdateUserDto) {
-    try {
-      const user: User = await this.usersService.update(req.user.sub, updateUserDto);
-      const { id, password, updatedAt, createdAt, ...response } = user;
-      return {
-        "data": {
-          response
-        },
-        "statusCode": 200,
-        "message": 'All account information has been successfully updated'
-      }
-    } catch (error: any) {
-      throw error;
+    @UseGuards(AuthenGuard)
+    @Put()
+    async update(@Req() req: any, @Body(new ValidationPipe({ whitelist: true })) updateUserDto: UpdateUserDto) {
+        try {
+            const user: User = await this.usersService.update(req.user.sub, updateUserDto);
+            const { id, password, updatedAt, createdAt, ...response } = user;
+            return {
+                data: {
+                    response
+                },
+                statusCode: 200,
+                message: 'All account information has been successfully updated'
+            }
+        } catch (error: any) {
+            if (error.statusCode === 401) {
+                throw new UnauthorizedException(error.message);
+            } else if (error.statusCode === 500) {
+                throw new InternalServerErrorException(error.message);
+            } else {
+                throw new BadRequestException(error.message);
+            }
+        }
     }
-  }
 
-  @UseGuards(AuthenGuard)
-  @Put('/updateAvatar')
-  @UseInterceptors(FileInterceptor('file'))
-  async updateAvatar(@Req() req: any, @UploadedFile() file: Express.Multer.File) {
-    try {
-      const imageUrl = await this.cloudStorageService.uploadImage(file, req.user.sub);
-      const updateUserDto: UpdateUserDto = {
-        avatarUrl: imageUrl
-      }
+    @UseGuards(AuthenGuard)
+    @Put('/updateAvatar')
+    @UseInterceptors(FileInterceptor('file'))
+    async updateAvatar(@Req() req: any, @UploadedFile() file: Express.Multer.File) {
+        try {
+            const imageUrl = await this.cloudStorageService.uploadImage(file, req.user.sub);
+            const updateUserDto: UpdateUserDto = {
+                avatarUrl: imageUrl
+            }
 
-      const extractAvatar: User = await this.usersService.findOne(req.user.sub);
-      const { avatarUrl, ...remains } = extractAvatar;
-      await this.cloudStorageService.deleteImage(avatarUrl);
+            const extractAvatar: User = await this.usersService.findOne(req.user.sub);
+            const { avatarUrl, ...remains } = extractAvatar;
+            await this.cloudStorageService.deleteImage(avatarUrl);
 
-      const user: User = await this.usersService.update(req.user.sub, updateUserDto);
-      return {
-        message: 'Avatar updated successfully',
-        statusCode: 200,
-        data: user.avatarUrl
-      };
-    } catch (error) {
-      throw new Error('Failed to change avatar. Please try again');
+            const user: User = await this.usersService.update(req.user.sub, updateUserDto);
+            return {
+                data: user.avatarUrl,
+                statusCode: 200,
+                message: 'Avatar updated successfully',
+            };
+        } catch (error) {
+            if (error.statusCode === 400) {
+                throw new BadRequestException(error.message);
+            } else if (error.statusCode === 401) {
+                throw new UnauthorizedException(error.message);
+            } else if (error.statusCode === 500) {
+                throw new InternalServerErrorException(error.message);
+            } else {
+                throw new NotImplementedException('Failed to change avatar. Please try again');
+            }
+        }
     }
-  }
 
-  @UseGuards(AuthenGuard)
-  @Delete()
-  async remove(@Req() req: any) {
-    try {
-      await this.usersService.remove(req.user.sub);
-      return {
-        "statusCode": 200,
-        "message": `User has been successfully deleted`
-      }
-    } catch (error: any) {
-      throw new ForbiddenException(`User can't be deleted`);
+    @UseGuards(AuthenGuard)
+    @Delete()
+    async remove(@Req() req: any) {
+        try {
+            await this.usersService.remove(req.user.sub);
+            return {
+                statusCode: 200,
+                message: `User has been successfully deleted`
+            }
+        } catch (error: any) {
+            if (error.statusCode === 500) {
+                throw new InternalServerErrorException(error.message);
+            } else {
+                throw new BadRequestException(error.message);
+            }
+        }
     }
-  }
+
+    @Post('logout')
+    async logout(
+        @Headers('Authorization') authHeader: string,
+    ): Promise<{ statusCode: number; message: string }> {
+        const token = authHeader.replace('Bearer ', '');
+        try {
+            await this.usersService.logout(token);
+            return {
+                statusCode: 200,
+                message: 'Logged out successfully',
+            };
+        } catch (error: any) {
+            if (error.statusCode === 401) {
+                throw new UnauthorizedException(error.message);
+            } else if (error.statusCode === 501) {
+                throw new NotImplementedException(error.message);
+            } else {
+                throw new BadRequestException(error.message);
+            }
+        }
+    }
 }
